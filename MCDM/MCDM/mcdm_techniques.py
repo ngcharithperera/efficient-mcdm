bb6ba20b06cf3d18bf4e71c33a08225bb38648da
Charith Perera <ngcharithperera@gmail.com>
2018-06-03 19:11:29 +0100

    added weighted prodcut
    added weighted prodcut

--- MCDM/MCDM/mcdm_techniques.py
+++ MCDM/MCDM/mcdm_techniques.py
@@ -1,6 +1,6 @@
 def weighted_sum_model(current_sensor_context_data, user_preferences):
   return sum([x*y for x,y in zip(current_sensor_context_data, user_preferences)])
   
-def weighted_product_model():
-  x = 0
+def weighted_product_model(current_sensor_context_data, user_preferences):
+  x = numpy.prod(numpy.asarray([x**y for x,y in zip(current_sensor_context_data, user_preferences)]))
   return x
